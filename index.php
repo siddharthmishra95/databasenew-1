989849e2cf8901f005f7c1f25a2a5a3a40009534
siddharthmishra95 <siiid.mishra@gmail.com>
2022-02-09 15:19:41 +0530

    user login control

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(!($_SESSION['name']==''))
       $email=$_POST["txt_uname_email"];
       $pass=$_POST["txt_password"];
       
-      $sql=mysqli_query($conn,"SELECT * from registration where email='$email' and password='$pass'");
+      $sql=mysqli_query($conn,"SELECT * from registration where email='$email' and password='$pass' AND usr_status = 1");
       if(mysqli_num_rows($sql))
       {
           while($row=mysqli_fetch_array($sql))
@@ -27,6 +27,7 @@ if(!($_SESSION['name']==''))
               $_SESSION["email"]=$email;
               $_SESSION["empid"]=$empid;
               $_SESSION['role'] = $row['role'];
+              $_SESSION['usr_status'] =$row['usr_status'];
               
           }
         header("location:recruiter/dashboard.php");
@@ -40,36 +41,36 @@ if(!($_SESSION['name']==''))
 ?>
 
 <body>
-  <div class="container-scroller">
-    <div class="container-fluid page-body-wrapper full-page-wrapper">
-      <div class="content-wrapper d-flex align-items-center auth">
-        <div class="row flex-grow">
-          <div class="col-lg-4 mx-auto">
-            <div class="auth-form-light text-left p-5">
-              <div class="brand-logo">
-                <img src="<?php echo BASE_URL; ?>assets/images/logo.svg">
-              </div>
-              <h4>Hello! let's get started</h4>
-              <h6 class="font-weight-light">Sign in to continue.</h6>
-              <form class="pt-3" method="post" id="login-form">
-                <div class="form-group">
-                  <input type="email" name="txt_uname_email" class="form-control form-control-lg"
-                    id="exampleInputEmail1" placeholder="Username" required="">
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth">
+                <div class="row flex-grow">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left p-5">
+                            <div class="brand-logo">
+                                <img src="<?php echo BASE_URL; ?>assets/images/logo.svg">
+                            </div>
+                            <h4>Hello! let's get started</h4>
+                            <h6 class="font-weight-light">Sign in to continue.</h6>
+                            <form class="pt-3" method="post" id="login-form">
+                                <div class="form-group">
+                                    <input type="email" name="txt_uname_email" class="form-control form-control-lg"
+                                        id="exampleInputEmail1" placeholder="Username" required="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="txt_password" class="form-control form-control-lg"
+                                        id="exampleInputPassword1" placeholder="Password" required="">
+                                </div>
+                                <div class="mt-3">
+                                    <button type="submit"
+                                        class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn"
+                                        name="btn-login">SIGN IN</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group">
-                  <input type="password" name="txt_password" class="form-control form-control-lg"
-                    id="exampleInputPassword1" placeholder="Password" required="">
-                </div>
-                <div class="mt-3">
-                  <button type="submit"
-                    class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn"
-                    name="btn-login">SIGN IN</button>
-                </div>
-              </form>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
-  <?php include ('shared/footer.php'); ?>
+    <?php include ('shared/footer.php'); ?>
